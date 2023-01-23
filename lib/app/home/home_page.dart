c51cfc2b1c4ad44306c365cd9fd27fdc2ca0a46e
Adrianna Gerek <aduzja@gmail.com>
2023-01-23 07:48:28 +0100

    Install cloud firestore

--- lib/app/home/home_page.dart
+++ lib/app/home/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:najlepszy_vet_warszawa/app/home/add_opinion/add_opinion_page_content.dart';
+import 'package:najlepszy_vet_warszawa/app/home/clinics/clinics_page_content.dart';
+import 'package:najlepszy_vet_warszawa/app/home/my_account/my_account_page_content.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({
@@ -19,32 +22,17 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Najlepszy weterynarz w Warszawie'),
+      ),
       body: Builder(builder: (context) {
         if (currentIndex == 0) {
-          return Center(
-            child: Text('Jeden'),
-          );
+          return const ClinicPageContent();
         }
         if (currentIndex == 1) {
-          return Center(
-            child: Text('Dwa'),
-          );
+          return const AddOpinionPageContent();
         }
-        return Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text('Jesteś zalogowany jako ${widget.user.email}'),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {
-                  FirebaseAuth.instance.signOut();
-                },
-                child: const Text('Wyloguj'),
-              ),
-            ],
-          ),
-        );
+        return MyAccountPageContent(email: widget.user.email);
       }),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: currentIndex,
@@ -71,3 +59,9 @@ class _HomePageState extends State<HomePage> {
     );
   }
 }
+
+
+
+
+
+
