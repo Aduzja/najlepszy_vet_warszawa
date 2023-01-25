b279e311c93e699091db41525739bf56675152d1
Adrianna Gerek <aduzja@gmail.com>
2023-01-25 23:12:18 +0100

    Move MyApp to app.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:najlepszy_vet_warszawa/app/app.dart';
 
-import 'package:najlepszy_vet_warszawa/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
